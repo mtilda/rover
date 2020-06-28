49d544cdbf8e196734c3e451b9aaf87213ddf0a5
mxkay <kayalyxvail@gmail.com>
2020-06-27 20:06:33 -0400

    added state hook currentImage

--- src/Components/RoverSight/RoverSight.js
+++ src/Components/RoverSight/RoverSight.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './RoverSight.css';
 
 const RoverSight = () => {
-    return <div className="rover-sight"></div>
+    const [currentImage, setCurrentImage] = useState('https://mars.nasa.gov/system/resources/detail_files/25058_PIA23900-web.jpg');
+
+    return (
+        <div className='rover-sight' style={{backgroundImage: `url(${currentImage})`}}>
+            
+        </div>
+    )
 }
 
 export default RoverSight;
