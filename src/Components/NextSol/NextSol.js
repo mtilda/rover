0361e9417f6fa6d31276578f86022540da3959ba
mxkay <kayalyxvail@gmail.com>
2020-06-29 19:34:05 -0400

    added NextSol component

---
+++ src/Components/NextSol/NextSol.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NextSol.css';
+
+const NextSol = () => {
+    return (
+        <div className='next-sol'>
+            <img src='https://imgur.com/cgoQOAS.png' alt='button: next sol' />
+        </div>
+    )
+}
+
+export default NextSol;
