56f7aca3d4c341b604be1296b17f61e4d5537579
mxkay <kayalyxvail@gmail.com>
2020-07-01 13:51:56 -0400

    fixed alt text in NextImage

--- src/Components/NextImage/NextImage.js
+++ src/Components/NextImage/NextImage.js
@@ -4,7 +4,7 @@ import './NextImage.css';
 const NextImage = ({ nextImage }) => {
     return (
         <div className='next-image' onClick={nextImage}>
-            <img src='https://imgur.com/cgoQOAS.png' alt='button: next sol' />
+            <img src='https://imgur.com/cgoQOAS.png' alt='button: next image' />
         </div>
     );
 }
